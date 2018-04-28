4941cd817231737a9b017678555c62eb2fb5bc9d
DillyB <dadielous@gmail.com>
2018-04-27 22:06:22 -0500

    Detect shapes

--- src/shape_detection.cpp
+++ src/shape_detection.cpp
@@ -3,62 +3,37 @@
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <iostream>
+#include <string>
 #include "shape_detection.h"
 #include <pcl/ModelCoefficients.h>
 #include <pcl/point_types.h>
 #include <pcl/features/normal_3d.h>
+#include <pcl/sample_consensus/ransac.h>
+#include <pcl/sample_consensus/sac_model_sphere.h>
+
 typedef pcl::PointXYZRGBA PointT;
 
 void shapeDetect(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud)
 {
-	// Get the model, if present.
-	//Cylinder
-	//-Objects
-	pcl::NormalEstimation<PointT, pcl::Normal> ne;
-	pcl::SACSegmentationFromNormals<PointT, pcl::Normal> seg;
-	pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT>());
+	std::string shape = "Undetermined";
 
-	//-Datasets
-	pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
-	pcl::ModelCoefficients::Ptr coefficients_cylinder (new pcl::ModelCoefficients);
-	pcl::PointIndices::Ptr inliers_cylinder (new pcl::PointIndices);
+	std::vector<int> inliersSph;
+	/*std::vector<int> inliersCyl;
 
-	//-Estimate point normals
-	ne.setSearchMethod(tree);
-	ne.setInputCloud(cloud);
-	ne.setKSearch(50);
-	ne.compute(*cloud_normals);
+        // created RandomSampleConsensus object and compute the appropriated model
+	pcl::SampleConsensusModelSphere<pcl::PointXYZRGBA>::Ptr
+	  model_s(new pcl::SampleConsensusModelSphere<pcl::PointXYZRGBA> (cloud));
 
-	//-Create segmentation object
-	seg.setOptimizeCoefficients(true);
-	seg.setMethodType(pcl::SAC_RANSAC);
-	seg.setDistanceThreshold(0.01);
-	seg.setNormalDistanceWeight(0.1);
-	seg.setRadiusLimits(0, 0.1);
-	seg.setInputCloud(cloud);
-	seg.setInputNormals(cloud_normals);
-	seg.setModelType(pcl::SACMODEL_CYLINDER);
-
-	//-Check if cylinder exists
-	seg.segment(*inliers_cylinder, *coefficients_cylinder);
-	if (inliers_cylinder->indices.size() != 0){
-		std::cout << "Cylinder" << std::endl;
-		return;
-	}
-/*
 	//Sphere
-	//-Set segmentation parameters
-	seg.setModelType(pcl::SACMODEL_NORMAL_SPHERE);
+	pcl::RandomSampleConsensus<pcl::PointXYZRGBA> ransacSph (model_s);
+	ransacSph.setDistanceThreshold (.01);
+	ransacSph.computeModel();
+	ransacSph.getInliers(inliersSph);
 
-	//-Check if sphere exists
-	pcl::PointIndices::Ptr shapeIndices(new pcl::PointIndices);
-	seg.segment(*shapeIndices, *coefficients_cylinder);
-	if (shapeIndices->indices.size() != 0){
-		std::cout << "Sphere" << std::endl;
-		return;
-	}
-*/
-
+	if(inliersSph.size() != 0)
+		//shape = "Sphere";
+	std::cout << "Sphere inliers (ransac): " << inliersSph.size() << std::endl;*/
+	
 	//Sphere
 	//-Objects
 	pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
@@ -79,18 +54,61 @@ void shapeDetect(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud)
 	segmentation.setModelType(pcl::SACMODEL_SPHERE);
 	segmentation.segment(*shapeIndices, *coefficients);
 	if (shapeIndices->indices.size() != 0){
-		std::cout << "Sphere" << std::endl;
-		return;
+		//shape = "Sphere";
+		//std::cout << "Sphere inliers (segment): " << shapeIndices->indices.size() << std::endl;
+		//return;
 	}
 
-/*
-	//Cylinder model
-	segmentation.setModelType(pcl::SACMODEL_CYLINDER);
-	segmentation.segment(*shapeIndices, *coefficients);
-	if (shapeIndices->indices.size() != 0){
-		std::cout << "Cylinder" << std::endl;
-		return;
+	//Cylinder
+	//-Objects
+	pcl::NormalEstimation<PointT, pcl::Normal> ne;
+	pcl::SACSegmentationFromNormals<PointT, pcl::Normal> seg;
+	pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT>());
+
+	//-Datasets
+	pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+	pcl::ModelCoefficients::Ptr coefficients_cylinder (new pcl::ModelCoefficients);
+	pcl::PointIndices::Ptr inliers_cylinder (new pcl::PointIndices);
+
+	//-Estimate point normals
+	ne.setSearchMethod(tree);
+	ne.setInputCloud(cloud);
+	ne.setKSearch(50);
+	ne.compute(*cloud_normals);
+
+	//-Create segmentation object
+	seg.setOptimizeCoefficients(true);
+	seg.setMethodType(pcl::SAC_RANSAC);
+	seg.setDistanceThreshold(0.12);//originally 0.01 //was 0.1, maybe too much now?
+	seg.setNormalDistanceWeight(0.1);
+	seg.setRadiusLimits(0, 0.5);//originally 0.1
+	seg.setInputCloud(cloud);
+	seg.setInputNormals(cloud_normals);
+	seg.setModelType(pcl::SACMODEL_CYLINDER);
+
+	//-Check if cylinder exists
+	seg.segment(*inliers_cylinder, *coefficients_cylinder);
+	if (inliers_cylinder->indices.size() != 0){
+		//shape = "Cylinder";
+		//std::cout << "Cylinder inliers (segment): " << inliers_cylinder->indices.size()<< std::endl;
+		//return;
 	}
-*/
-	std::cout << "Undetermined" << std::endl;
+
+//compare which model has more inliers, if either
+	if(shapeIndices->indices.size() > inliers_cylinder->indices.size() || shapeIndices->indices.size() == inliers_cylinder->indices.size())
+		shape = "Sphere";
+	else if(inliers_cylinder->indices.size() > inliersSph.size())
+		shape = "Cylinder";
+	
+
+
+
+
+        //
+        //std::cout << "Sphere Inliers: " << inliersSph.size() << std::endl;
+        ////std::cout << "Cylinder Inliers: " << inliersCyl.size() << std::endl;
+        //
+
+	std::cout << shape << std::endl;
+	//std::cout << "Undetermined" << std::endl;
 }
